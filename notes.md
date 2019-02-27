0c16ac3fd8ead35ae7a2513e0ca227058a3c1c8d
forral <luis.forra@celfocus.com>
2019-02-27 17:28:53 +0000

    dinamic add li elements into the client list page, add a note file and start to refactor the listeners on the client list

---
+++ notes.md
@@ -0,0 +1,11 @@
+http://www.ctt.pt/pdcp/xml_pdcp?inlocal=casal+de+cambra&inrua=rua+palma
+http://www.ctt.pt/pdcp/xml_pdcp?inlocal=almada&inrua=praca+gil+vicente
+
+IDEAS:
+- send SMS
+- send EMAIL
+- Customer Interactions (guardar informação de toda interacção com cliente. expl: correios enviados, sms enviados, etc...)
+
+https://www.ctt.pt/contentAsset/raw-data/09ae68cd-de81-40f1-9dd6-28728b0bbefd/ficheiro/f9b2b545-2002-44a6-9282-d04d6dcab63a/export/man_util_xml_v16.pdf
+
+https://www.portugal-a-programar.pt/forums/topic/69470-web-service-moradas-e-codigos-postais/
--- xpto-component/clientList/clientList.js
+++ xpto-component/clientList/clientList.js
@@ -1,30 +1,46 @@
 var clientList = (function() {
 
-    function registerListeners() {
-        var clientList = document.querySelectorAll('#client-list li');
-        clientList = [...clientList];
+    var mockdataClientList = [
+        {"id": "001", "name": "Pablo Aimar", "address": "Rua dos Pinheiros, Aroeira", "email": "aimar@omaior.com", "phone": "+351928765643", "NIF": "982982722"},
+        {"id": "002", "name": "Rui Costa", "address": "Parque dos Principes, Telheiras", "email": "rui_costa@omaior.com", "phone": "+351965434554", "NIF": "552982711"},
+        {"id": "003", "name": "Nico Gaitan", "address": "Rua do Ouro, Lisboa", "email": "gaitan@omaior.com", "phone": "+351938665643", "NIF": "412672722"},
+        {"id": "004", "name": "Angel Di Maria", "address": "Av. D. João I, Almada", "email": "di_maria@omaior.com", "phone": "+351928711633", "NIF": "002982722"},
+        {"id": "005", "name": "Ariel Ortega", "address": "Rua Capitão Leitão, Almada", "email": "burrito@omaior.com", "phone": "+351914465643", "NIF": "982982722"}
+    ]
 
-        clientList.forEach(function (client) {
+    var app = document.querySelector('#app');
+    var clientPageURL = '../client-page/client.html?id=';
+
+    function registerListeners() {
+        var clientList = document.querySelector('#client-list');
+        // clientList = [...clientList];
+
+        /*clientList.forEach(function (client) {
             client.addEventListener('click', function (client) {
-                window.location.replace('file:///Users/luisforra/workspace/xpto-frontend/xpto-component/client-page/client.html?id=' + client.target.id);
+                window.location.replace(clientPageURL + client.target.id);  
             });
+        });*/
+
+
+        clientList.addEventListener('click', function(event) {
+
+            console.log(event);
+
         });
+
     }
 
     function templateClientList(clientList) {
         return `<ul id="client-list">
-                    <li id="${clientList.id}">${clientList.name}</li>
-                    <li id="${clientList.id}">${clientList.name}</li>
-                    <li id="${clientList.id}">${clientList.name}</li>
-                    <li id="${clientList.id}">${clientList.name}</li>
-                    <li id="${clientList.id}">${clientList.name}</li>
-                    <li id="${clientList.id}">${clientList.name}</li>
+                    ${clientList.map(client => `<li id="${client.id}">${client.name}</li>`).join('')}
                 </ul>`;
     }
 
     function init() {
         // 1. http request
-        // 2. template
+        // 2. template [DONE]
+        // 3. listeners [DONE] TODO: buble
+        app.innerHTML = templateClientList(mockdataClientList);
         registerListeners();
     }
 
